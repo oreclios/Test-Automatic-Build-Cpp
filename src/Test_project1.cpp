333649a591c2c5d589537d3b90381f01be0509bd
oreclios <oreclios@gmail.com>
2017-01-13 11:47:32 +0100

    Prueba commit 2.

--- src/Test_project1.cpp
+++ src/Test_project1.cpp
@@ -11,5 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Esto es una prueba." << endl; // prints !!!Hello World!!!
 	return 0;
 }
